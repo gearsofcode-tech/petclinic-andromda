e20ca45b61ff66f15980510b429638140984400b
Carlos Padoa <samuraicharlie@gearsofcode.tech>
2022-08-15 13:48:50 -0300

    Increment first template (add table)

--- web/src/main/frontend/src/App.vue
+++ web/src/main/frontend/src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/about">Veterinarian</RouterLink>
+        <RouterLink to="/vet">Veterinarian</RouterLink>
       </nav>
     </div>
   </header>
--- web/src/main/frontend/src/views/VeterinarianView.vue
+++ web/src/main/frontend/src/views/VeterinarianView.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="about">
+  <div>
     <h1>Veterinarian</h1>
+    <br/>
+    <table>
+    <tr>
+      <th>First name</th>
+      <th>Last name</th>
+    </tr>
+    <tr v-for="vet in vets">
+      <td>{{ vet.firstName }}</td>
+      <td>{{ vet.lastName }}</td>
+    </tr>
+  </table>
   </div>
+ 
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<script>
+export default{
+  data() {
+    return {
+      vets: [{ firstName: 'John', lastName: 'Graham' }, { firstName: 'Kelly', lastName: 'Morrison' }]
+    }
   }
 }
-</style>
+</script>
